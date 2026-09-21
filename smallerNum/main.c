32693ef10d62d142d7b7d83c40257048ba97a33c
Manan <joshi.manan@gmail.com>
2026-09-21 09:33:44 +0530

    ishan

---
+++ smallerNum/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int checkForValidNum();
+
+int main(){
+
+    int num1, num2;
+
+    printf("Enter first number\n>> ");
+    num1 = checkForValidNum();
+
+    printf("Enter second number\n>> ");
+    num2 = checkForValidNum();
+
+    if(num1>num2){
+        printf("%d is smaller",num2);
+    }
+    else if(num2>num1){
+        printf("%d is smaller",num1);
+    }
+    else{
+        printf("Equal numbers");
+    }
+
+    return 0;
+
+}
+
+int checkForValidNum(){
+    int num;
+    while(scanf("%d",&num) != 1){
+        int c;
+        while((c = getchar()) != '\n' && c != EOF);
+        printf("Invalid input, Try again\nEnter your number\n>> ");
+    }
+    return num;
+}
