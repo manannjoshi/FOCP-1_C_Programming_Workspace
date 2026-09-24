b1cc4e7711d0e361d2a8e8bef3b3352216382d5b
Manan <joshi.manan@gmail.com>
2026-09-24 21:03:45 +0530

    assignment 2++

--- Assignment 2/electricityBillSlab/main.c
+++ Assignment 2/electricityBillSlab/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+ 
+int main(){
 
-int main() {
+    int unit;
+    int rate;
+
+    printf("Enter your number\n>> ");
+    scanf("%d",&unit);
+
+    if(unit>=0 && unit<=100){
+        rate = unit*2;
+        printf("%d",rate);
+    }
+    else if(unit>=101 && unit<=200){
+        rate = unit*3;
+        printf("%d",rate);
+    }
+    else{
+        rate = unit*5;
+        printf("%d",rate);
+    }
 
     return 0;
 }
--- Assignment 2/evenOddOrSpecial/main.c
+++ Assignment 2/evenOddOrSpecial/main.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 
+int checkForValidInt();
+ 
 int main() {
 
+    int n;
+    printf("Enter your number\n>> ");
+    n = checkForValidInt();
+
+    if(n%2==0 && n%5==0){
+        printf("Special");
+    }
+    else if(n%2==0){
+        printf("Even");
+    }
+    else if(n%5==0){
+        printf("Five");
+    }
+    else{
+        printf("Odd/Other");
+    }
+
     return 0;
+}
+
+int checkForValidInt(){
+    int num;
+    while(scanf("%d",&num) != 1){
+        int c;
+        while((c=getchar() != '\n' && c != EOF));
+        printf("Invalid input, try again\n>> ");
+    }
+    return num;
 }
